f8ea9f0e77ebd1e468d944ed079a8ec44072c99c
OllaWilk <aleksandra.wilk.berserksoft@gmail.com>
2025-01-21 09:21:28 +0100

    contentSwitcherCart

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import { useState } from "react";
-import "./styles.css";
+import { useState } from 'react';
+import './styles.css';
 
 const content = [
   [
-    "React is extremely popular",
-    "It makes building complex, interactive UIs a breeze",
+    'React is extremely popular',
+    'It makes building complex, interactive UIs a breeze',
     "It's powerful & flexible",
-    "It has a very active and versatile ecosystem",
+    'It has a very active and versatile ecosystem',
   ],
   [
-    "Components, JSX & Props",
-    "State",
-    "Hooks (e.g., useEffect())",
-    "Dynamic rendering",
+    'Components, JSX & Props',
+    'State',
+    'Hooks (e.g., useEffect())',
+    'Dynamic rendering',
   ],
   [
-    "Official web page (react.dev)",
-    "Next.js (Fullstack framework)",
-    "React Native (build native mobile apps with React)",
+    'Official web page (react.dev)',
+    'Next.js (Fullstack framework)',
+    'React Native (build native mobile apps with React)',
   ],
   [
-    "Vanilla JavaScript requires imperative programming",
-    "Imperative Programming: You define all the steps needed to achieve a result",
-    "React on the other hand embraces declarative programming",
-    "With React, you define the goal and React figures out how to get there",
+    'Vanilla JavaScript requires imperative programming',
+    'Imperative Programming: You define all the steps needed to achieve a result',
+    'React on the other hand embraces declarative programming',
+    'With React, you define the goal and React figures out how to get there',
   ],
 ];
 
@@ -33,41 +33,41 @@ export default function App() {
   return (
     <div>
       <header>
-        <img src="react-logo-xs.png" alt="React logo" />
+        <img src='react-logo-xs.png' alt='React logo' />
         <div>
           <h1>React.js</h1>
           <p>i.e., using the React library for rendering the UI</p>
         </div>
       </header>
 
-      <div id="tabs">
+      <div id='tabs'>
         <menu>
           <button
-            className={activeContentIndex === 0 ? "active" : ""}
+            className={activeContentIndex === 0 ? 'active' : ''}
             onClick={() => setActiveContentIndex(0)}
           >
             Why React?
           </button>
           <button
-            className={activeContentIndex === 1 ? "active" : ""}
+            className={activeContentIndex === 1 ? 'active' : ''}
             onClick={() => setActiveContentIndex(1)}
           >
             Core Features
           </button>
           <button
-            className={activeContentIndex === 2 ? "active" : ""}
+            className={activeContentIndex === 2 ? 'active' : ''}
             onClick={() => setActiveContentIndex(2)}
           >
             Related Resources
           </button>
           <button
-            className={activeContentIndex === 3 ? "active" : ""}
+            className={activeContentIndex === 3 ? 'active' : ''}
             onClick={() => setActiveContentIndex(3)}
           >
-            New Button
+            React vs JS
           </button>
         </menu>
-        <div id="tab-content">
+        <div id='tab-content'>
           <ul>
             {content[activeContentIndex].map((item) => (
               <li key={item}>{item}</li>
