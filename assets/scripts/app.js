12fcb8c050a69b7ae80ddb21c077b466970476c1
OllaWilk <aleksandra.wilk.berserksoft@gmail.com>
2025-01-27 13:00:56 +0100

    destrukturyzacja tablic i obiektów

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,46 @@
-const arrNumbs = [1, 2, 3];
+const arrNumbs = ['a', 'b', 'c'];
 
 function transformToObjects(numberArray) {
   return numberArray.map((numEl) => ({ val: numEl }));
 }
 
 console.log(transformToObjects(arrNumbs));
+
+// wyszukiwanie i pobieranie danych w tablicy
+
+/* 1 użycie numeru indexu */
+// const userNameData = ['Max', 'Doe'];
+// const firstName = userNameData[0];
+// const lastName = userNameData[1];
+
+/* 2 skrócona wersja destrukturyzacja*/
+const [firstName, lastName] = ['Max', 'Doe'];
+
+// destrukturyzacja dla obiektu
+
+/* 1 wyciąganie z obiektu  */
+// const user = {
+//     name: "Ola",
+//     age: 34
+// }
+
+//const name = user.name
+//const age= user.age
+
+/* 2 destrukturyzacja obiektu */
+const { name: userName, age } = {
+  name: 'Ola',
+  age: 34,
+};
+
+console.log(userName, age);
+
+/* DESTRUKTURYZACJA W PARAMETRZE FUNKCJI */
+function storeOrder({ id, currency, amount }) {
+  localStorage.setItem('id', id);
+  localStorage.setItem('currency', currency);
+
+  const storageCheck = localStorage.getItem('id');
+  console.log(storageCheck);
+}
+storeOrder({ id: 5, currency: 'USD', amount: 15.99 });
--- dist/app.0eb78c48.js
+++ dist/app.0eb78c48.js
@@ -118,7 +118,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"assets/scripts/app.js":[function(require,module,exports) {
-var arrNumbs = [1, 2, 3];
+var arrNumbs = ['a', 'b', 'c'];
 function transformToObjects(numberArray) {
   return numberArray.map(function (numEl) {
     return {
@@ -127,6 +127,53 @@ function transformToObjects(numberArray) {
   });
 }
 console.log(transformToObjects(arrNumbs));
+
+// wyszukiwanie i pobieranie danych w tablicy
+
+/* 1 użycie numeru indexu */
+// const userNameData = ['Max', 'Doe'];
+// const firstName = userNameData[0];
+// const lastName = userNameData[1];
+
+/* 2 skrócona wersja destrukturyzacja*/
+var firstName = 'Max',
+  lastName = 'Doe';
+
+// destrukturyzacja dla obiektu
+
+/* 1 wyciąganie z obiektu  */
+// const user = {
+//     name: "Ola",
+//     age: 34
+// }
+
+//const name = user.name
+//const age= user.age
+
+/* 2 destrukturyzacja obiektu */
+var _name$age = {
+    name: 'Ola',
+    age: 34
+  },
+  userName = _name$age.name,
+  age = _name$age.age;
+console.log(userName, age);
+
+/* DESTRUKTURYZACJA W PARAMETRZE FUNKCJI */
+function storeOrder(_ref) {
+  var id = _ref.id,
+    currency = _ref.currency,
+    amount = _ref.amount;
+  localStorage.setItem('id', id);
+  localStorage.setItem('currency', currency);
+  var storageCheck = localStorage.getItem('id');
+  console.log(storageCheck);
+}
+storeOrder({
+  id: 5,
+  currency: 'USD',
+  amount: 15.99
+});
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
