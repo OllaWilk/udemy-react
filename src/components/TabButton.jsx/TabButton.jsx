6d3a167c5012b7a73d467b3429303f68caa38339
OllaWilk <aleksandra.wilk.berserksoft@gmail.com>
2025-02-03 11:59:13 +0100

    dodanie nasłuchiwania na klik

--- src/components/TabButton.jsx/TabButton.jsx
+++ src/components/TabButton.jsx/TabButton.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 export const TabButton = ({ children }) => {
+  function handleClick() {
+    console.log('hello');
+  }
   return (
     <li>
-      <button>{children}</button>
+      <button onClick={handleClick}>{children}</button>
     </li>
   );
 };
